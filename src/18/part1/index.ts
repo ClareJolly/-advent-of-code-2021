2bbd2b315fda83482fb7402962886f718ec7aacb
Clare Jolly <clajollyuk@gmail.com>
2021-12-15 22:57:35 +0000

    part 18 setup

---
+++ src/18/part1/index.ts
@@ -0,0 +1,7 @@
+const part1 = (inputData: string[]) => {
+  const data = inputData.map(d => d.split(''))
+  console.log('🚀 ~ file: index.ts ~ line 3 ~ part1 ~ data', data)
+  const steps = 4
+}
+
+export default part1
