0a55efcc4be263c8b672c65aa8a8d753d92b1101
Clare Jolly <clajollyuk@gmail.com>
2021-12-15 23:47:52 +0000

    part 18 part 1 complete

--- src/18/part2/index.ts
+++ src/18/part2/index.ts
@@ -1,3 +1,73 @@
-const part2 = (inputData: string[]) => {}
+export const ADJACENT_CONFIG: number[][] = [
+  [-1, 0],
+  [1, 0],
+  [0, -1],
+  [0, 1],
+  [1, 1],
+  [1, -1],
+  [-1, 1],
+  [-1, -1],
+]
+
+interface Details {
+  on: boolean
+  adjacent: string[]
+}
+interface Summary {
+  [key: string]: Details
+}
+
+const part2 = (inputData: string[]) => {
+  const data = inputData.map(d => d.split(''))
+
+  const height = data.length
+  const width = data[0].length
+
+  const stuckOn = [`0,0`, `0,${width}`, `${height},0`, `${height},${width}`]
+  //   const steps = 100
+  const steps = 5
+
+  const summary: Summary = data.reduce((acc, row, y) => {
+    row.forEach((r, x) => {
+      let adj: string[] = []
+
+      ADJACENT_CONFIG.forEach(([yAdj, xAdj]) => {
+        if (data[y + yAdj] && data[y + yAdj][x + xAdj]) adj.push(`${y + yAdj},${x + xAdj}`)
+      })
+
+      acc[`${y},${x}`] = { on: r === '#', adjacent: adj }
+    })
+    return acc
+  }, {} as Summary)
+
+  for (let step = 0; step < steps; step++) {
+    const onQueue = []
+    const offQueue = []
+
+    const onCheck = (coOrd: string) => !summary[coOrd].on
+    const offCheck = (coOrd: string) => summary[coOrd].on
+
+    Object.entries(summary).forEach(([key, { on, adjacent }]) => {
+      let adjOnCount = 0
+      adjacent.forEach(c => {
+        if (summary[c].on) adjOnCount++
+      })
+      if (on && (adjOnCount === 2 || adjOnCount === 3)) onQueue.push(key)
+      if (!on && adjOnCount === 3) onQueue.push(key)
+      if (!stuckOn.includes(key)) {
+        if (on && adjOnCount !== 2 && adjOnCount !== 3) offQueue.push(key)
+        if (!on && adjOnCount !== 3) offQueue.push(key)
+      }
+    })
+    onQueue.forEach(q => {
+      summary[q].on = true
+    })
+    offQueue.forEach(q => {
+      summary[q].on = false
+    })
+  }
+
+  return Object.entries(summary).filter(([_, { on }]) => on).length
+}
 
 export default part2
