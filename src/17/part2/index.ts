945e75db200dad1a6a45a83158ac1f12ca8a5fd2
Clare Jolly <clajollyuk@gmail.com>
2021-12-15 22:48:05 +0000

    part 17 setup and part 1 complete

--- src/17/part2/index.ts
+++ src/17/part2/index.ts
@@ -1,3 +1,42 @@
-const part2 = (inputData: string[]) => {}
+import { arrToNumbers } from '../../helpers'
+
+interface Counter {
+  total: number
+  n: number
+  i?: number
+  data: number[]
+}
+
+const counter = ({ total, n, i, data }: Counter): number => {
+  i = i || 0
+
+  if (n < 0) {
+    return 0
+  } else if (total === 0) {
+    return 1
+  } else if (i === data.length || total < 0) {
+    return 0
+  } else {
+    return (
+      counter({ total, n, i: i + 1, data }) +
+      counter({ total: total - data[i], n: n - 1, i: i + 1, data })
+    )
+  }
+}
+
+const part2 = (inputData: string[]) => {
+  const data = arrToNumbers(inputData)
+
+  const total = 150
+
+  //   return counter({ total, n: data.length, data })
+
+  var i = 1,
+    result
+  while (!result) {
+    result = counter({ total, n: i++, data })
+  }
+  return result
+}
 
 export default part2
