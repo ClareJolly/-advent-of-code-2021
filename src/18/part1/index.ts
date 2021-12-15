e0dc8b91acf3d95d77a47e9e81e9d61c903c97da
Clare Jolly <clajollyuk@gmail.com>
2021-12-15 23:41:32 +0000

    part 18 part 1 complete

--- src/18/part1/index.ts
+++ src/18/part1/index.ts
@@ -1,7 +1,65 @@
+export const ADJACENT_CONFIG: number[][] = [
+  [-1, 0],
+  [1, 0],
+  [0, -1],
+  [0, 1],
+  [1, 1],
+  [1, -1],
+  [-1, 1],
+  [-1, -1],
+]
+
+interface Details {
+  on: boolean
+  adjacent: string[]
+}
+interface Summary {
+  [key: string]: Details
+}
+
 const part1 = (inputData: string[]) => {
   const data = inputData.map(d => d.split(''))
-  console.log('🚀 ~ file: index.ts ~ line 3 ~ part1 ~ data', data)
-  const steps = 4
+  const steps = 100
+
+  const summary: Summary = data.reduce((acc, row, y) => {
+    row.forEach((r, x) => {
+      let adj: string[] = []
+
+      ADJACENT_CONFIG.forEach(([yAdj, xAdj]) => {
+        if (data[y + yAdj] && data[y + yAdj][x + xAdj]) adj.push(`${y + yAdj},${x + xAdj}`)
+      })
+
+      acc[`${y},${x}`] = { on: r === '#', adjacent: adj }
+    })
+    return acc
+  }, {} as Summary)
+
+  for (let step = 0; step < steps; step++) {
+    const onQueue = []
+    const offQueue = []
+
+    const onCheck = (coOrd: string) => !summary[coOrd].on
+    const offCheck = (coOrd: string) => summary[coOrd].on
+
+    Object.entries(summary).forEach(([key, { on, adjacent }]) => {
+      let adjOnCount = 0
+      adjacent.forEach(c => {
+        if (summary[c].on) adjOnCount++
+      })
+      if (on && (adjOnCount === 2 || adjOnCount === 3)) onQueue.push(key)
+      if (on && adjOnCount !== 2 && adjOnCount !== 3) offQueue.push(key)
+      if (!on && adjOnCount === 3) onQueue.push(key)
+      if (!on && adjOnCount !== 3) offQueue.push(key)
+    })
+    onQueue.forEach(q => {
+      summary[q].on = true
+    })
+    offQueue.forEach(q => {
+      summary[q].on = false
+    })
+  }
+
+  return Object.entries(summary).filter(([_, { on }]) => on).length
 }
 
 export default part1
